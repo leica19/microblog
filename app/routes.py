dee8f21c3e82f8292fcbc4240e9f3036fd833b56
leica19 <spike-19@ezweb.ne.jp>
2018-05-11 18:09:01 +0900

    added a new page.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 
 from flask import render_template, flash, redirect, url_for, request
-from app import app
+from app import app, db
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from app.forms import RegistrationForm
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -27,9 +28,33 @@ def login():
 @app.route('/index')
 @login_required
 def index():
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
