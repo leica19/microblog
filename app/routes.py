2c80bf93c07e524e5114cc520b56a98c1b9cf2e8
leica19 <spike-19@ezweb.ne.jp>
2018-05-12 10:05:17 +0900

    added a user page.

--- app/routes.py
+++ app/routes.py
@@ -58,3 +58,14 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'okinawa is super! #!'},
+        {'author': user, 'body': 'Mr.shigaki is so cool! #2'},
+        {'author': user, 'body': 'i neee you, baby.. #3'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
